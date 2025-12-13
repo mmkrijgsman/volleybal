cf39cce21f362dddddda24fcf7d57b0e004c13a9
mmkrijgsman <m3krijgsman@gmail.com>
2025-12-14 00:20:12 +0100

    Delete src directory

--- src/VolleyballTracker.jsx
+++
@@ -1,8 +0,0 @@
-export default function VolleyballTracker() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>🏐 Volleyball Match Tracker</h1>
-      <p>Project scaffold – klaar voor uitbreiding.</p>
-    </div>
-  );
-}
