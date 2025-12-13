df455b22e261f74a355d843575bbdd5fd0a9821d
mmkrijgsman <m3krijgsman@gmail.com>
2025-12-14 00:59:15 +0100

    Update VolleyballTracker.jsx

---
+++ src/VolleyballTracker.jsx
@@ -0,0 +1,10 @@
+import PlayersPanel from './components/Players/PlayersPanel';
+
+export default function VolleyballTracker() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>🏐 Volleyball Match Tracker</h1>
+      <PlayersPanel />
+    </div>
+  );
+}
